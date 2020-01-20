25ff49e225321737f845eaf10274a50620dbfcf1
tektohub <boudouros@gmail.com>
2020-01-20 10:20:35 +0100

    1 file modified

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 import { Input } from '@angular/core';
 
+import { Output, EventEmitter } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-product-alerts',
